169a3a8f1745f43dbe73b9d26158308d51f82b1a
Fadhilahlaras <fadhilahlaras97@gmail.com>
2023-08-30 16:57:37 +0700

    Modify about us

--- src/DemoPages/AboutUs/index.js
+++ src/DemoPages/AboutUs/index.js
@@ -13,6 +13,10 @@ import AppHeader from "../../Layout/AppHeader";
 import AppFooter from "../../Layout/AppFooter";
 import {Link} from "react-router-dom";
 
+import pro1 from "../../assets/utils/images/product/kaos/meteora1.jpeg";
+import pro2 from "../../assets/utils/images/product/kaos/pokemon1.jpeg";
+import pro3 from "../../assets/utils/images/product/kaos/pokemon6.jpeg";
+
 
 export default class AboutUs extends Component {
 
@@ -75,7 +79,7 @@ export default class AboutUs extends Component {
                     <div className="app-main">
                         <Row className="h-100">
                             <Row className="h-100 no-gutters">
-                                <Col lg="10" md="12"
+                                <Col lg="6" md="12"
                                      className="h-100 d-flex bg-white justify-content-center align-items-center">
                                     <Col lg="9" md="10" sm="12" className="mx-auto app-login-box">
 
@@ -120,6 +124,67 @@ export default class AboutUs extends Component {
                                         </div>
 
                                     </Col>
+                                </Col>
+                                <Col lg="4" className="d-none d-lg-block">
+                                    <div className="slider-light">
+                                        <Slider  {...settings}>
+                                            {/*<div*/}
+                                            {/*    className="h-100 d-flex justify-content-center align-items-center bg-premium-dark">*/}
+                                            {/*    <div className="slide-img-bg"*/}
+                                            {/*         style={{*/}
+                                            {/*             backgroundImage: 'url(' + bg3 + ')'*/}
+                                            {/*         }}/>*/}
+                                            {/*    <div className="slider-content">*/}
+                                            {/*        <h3>Scalable, Modular, Consistent</h3>*/}
+                                            {/*        <p>*/}
+                                            {/*            Easily exclude the components you don't require. Lightweight,*/}
+                                            {/*            consistent*/}
+                                            {/*            Bootstrap based styles across all elements and components*/}
+                                            {/*        </p>*/}
+                                            {/*    </div>*/}
+                                            {/*</div>*/}
+                                            <div className="h-100 d-flex justify-content-center align-items-center bg-plum-plate">
+                                                <div className="slide-img-bg"
+                                                     style={{
+                                                         backgroundImage: 'url(' + pro1 + ')'
+                                                     }}/>
+                                                <div className="slider-content">
+                                                    <h3>Perfect Bright</h3>
+                                                    <p>
+                                                        Daily You is like a dream. This product can help you to brighten your face and clean your face optimally from dust, dirt, and remaining makeup.
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <div
+                                                className="h-100 d-flex justify-content-center align-items-center bg-premium-dark">
+                                                <div className="slide-img-bg"
+                                                     style={{
+                                                         backgroundImage: 'url(' + pro2 + ')'
+                                                     }}/>
+                                                <div className="slider-content">
+                                                    <h3>Nature Daily</h3>
+                                                    <p>
+                                                        Contains natural Aloe vera which provides extra moisture with a soothing effect and cools dry and reddened skin due to the sun.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div
+                                                className="h-100 d-flex justify-content-center align-items-center bg-sunny-morning">
+                                                <div className="slide-img-bg opacity-6"
+                                                     style={{
+                                                         backgroundImage: 'url(' + pro3 + ')'
+                                                     }}/>
+                                                <div className="slider-content">
+                                                    <h3>C Defense</h3>
+                                                    <p>
+                                                        Free radicals can damage the skin. Starting from making it dry, dull, accelerate aging to acne. For that you need anti-oxidant skincare that can win free radicals so that your skin is healthy and glowing.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </Slider>
+                                    </div>
+
                                 </Col>
                             </Row>
 
